ded65e02e295873fb71a3b8c414b7be7a8062099
Drakalyebus <mishastelephonom@gmail.com>
2025-02-23 22:40:33 +0300

    dz13

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 import Req from "./requests.js";
 
+const tasksEl = document.getElementById("tasks");
 const postEl = document.getElementById("post");
-const getEl = document.getElementById("get");
-const patchEl = document.getElementById("patch");
-const deleteEl = document.getElementById("delete");
-const listEl = document.getElementById("list");
 
-postEl.addEventListener("click", async (e) => {
-    e.preventDefault();
-    await Req.post("http://localhost:3000/products", {
-        image: prompt("Введите ссылку на картинку"),
-        name: prompt("Введите название"),
-        price: +prompt("Введите стоимость"),
-        quantity: +prompt("Введите количество"),
-        id: prompt("Введите id")
+(async () => {
+    const tasks = await Req.get("http://localhost:3000/TASKS");
+    tasksEl.innerHTML = '';
+    tasks.forEach((task) => {
+        tasksEl.innerHTML += `<li ${task.done ? 'style="text-decoration: line-through;"' : ''}>${task.title} - ${task.description} - ${task.date}</li>`
     });
-});
-
-getEl.addEventListener("click", async (e) => {
-    e.preventDefault();
-    const data = await Req.get("http://localhost:3000/products");
-    listEl.innerHTML = '';
-    data.forEach((el) => {
-        listEl.innerHTML += `<li><img src="${el.image}"><span>${el.name}</span><span>${el.price}$ x ${el.quantity}</span></li>`;
+    const tasksEls = document.querySelectorAll('li');
+    tasksEls.forEach((taskEl, i) => {
+        const task = tasks[i];
+        taskEl.addEventListener("click", async () => {
+            await Req.delete("http://localhost:3000/TASKS", task.id);
+            location.reload();
+        });
+        taskEl.addEventListener("contextmenu", async (e) => {
+            e.preventDefault();
+            await Req.patch("http://localhost:3000/TASKS", task.id, {done: !task.done});
+            location.reload();
+        });
     });
-});
+})();
 
-patchEl.addEventListener("click", async (e) => {
-    e.preventDefault();
-    await Req.patch("http://localhost:3000/products", +prompt("Введите id"), {
-        image: prompt("Введите ссылку на картинку"),
-        name: prompt("Введите название"),
-        price: +prompt("Введите стоимость"),
-        quantity: +prompt("Введите количество"),
-        id: +prompt("Введите id")
-    });
-});
-
-deleteEl.addEventListener("click", async (e) => {
-    e.preventDefault();
-    await Req.delete("http://localhost:3000/products", +prompt("Введите id"));
+postEl.addEventListener("click", async () => {
+    await Req.post("http://localhost:3000/TASKS", {title: prompt("Введите название задачи"), description: prompt("Введите описание задачи"), date: new Date().toLocaleDateString('ru-RU'), done: false});
+    location.reload();
 });
